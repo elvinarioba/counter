40b33d4c9814da08112174b079df97dd1b97d7d8
elvinarioba <88180411+elvinarioba@users.noreply.github.com>
2022-06-23 23:04:44 +0300

    Fist Commit

--- assets/main.js
+++ assets/main.js
@@ -6,3 +6,27 @@ button5.addEventListener('click', () => {
   disp.textContent = "added 5"
   indicator.textContent = (parseInt(indicator.textContent) + 5).toString()
 })
+const button10 = document.querySelector("#plusTen")
+const indicator10 = document.querySelector("#number-indicator")
+const disp10 = document.querySelector("#display-heading")
+
+button10.addEventListener('click', () => {
+  disp.textContent = "added 10"
+  indicator.textContent = (parseInt(indicator.textContent) + 10).toString()
+})
+const button15 = document.querySelector("#plusFifteen")
+const indicator15 = document.querySelector("#number-indicator")
+const disp15 = document.querySelector("#display-heading")
+
+button15.addEventListener('click', () => {
+  disp.textContent = "added 15"
+  indicator.textContent = (parseInt(indicator.textContent) + 15).toString()
+})
+const button20 = document.querySelector("#plusTwenty")
+const indicator20 = document.querySelector("#number-indicator")
+const disp20 = document.querySelector("#display-heading")
+
+button20.addEventListener('click', () => {
+  disp.textContent = "added 20"
+  indicator.textContent = (parseInt(indicator.textContent) + 20).toString()
+})
